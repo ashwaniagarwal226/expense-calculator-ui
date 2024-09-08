fa2cdbf8d67e25976df6d22b839fc33b5e1a9d26
ashwiniagarwal <ashwaniagarwal226@gmail.com>
2024-09-08 17:16:50 +0530

    getting graph data from api after upload

--- src/components/UploadPage.jsx
+++ src/components/UploadPage.jsx
@@ -1,12 +1,65 @@
 import { useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
+import axios from "axios";
 
 const fileTypes = ["xls"];
 
 export default function UploadPage() {
   const [file, setFile] = useState(null);
-  const handleChange = (file) => {
+  const [message, setMessage] = useState(null);
+  const [monthlyData, setMonthlyData] = useState([]);
+
+  const uploadFileToServer = async (file) => {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const response = await axios.post("http:///192.168.1.3:8081/api/expense/v1/hdfc/transactionupload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      setMessage(response.data.status);
+      return true; 
+    } catch (error) {
+      console.error("Error uploading file:", error);
+      setMessage("Failed to upload file.");
+      return false; 
+    }
+
+  };
+
+  const getGraphData = async () => {
+    try {
+      const response = await axios.get("http:///192.168.1.3:8081/api/expense/v1/hdfc/graphdata");
+      setMonthlyData(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error calling API:", error);
+    }
+  };
+
+  const handleChange = async (file) => {
     setFile(file);
+    const uploadSuccess = await uploadFileToServer(file);
+    if (uploadSuccess) {
+      await getGraphData();
+    }
+
+  };
+
+  const getPieChartData = (transSummary) => {
+    // Initialize chart data with headers for Google Charts
+    const chartData = [["Transaction Type", "Total Amount"]];
+    
+    // Loop through the transaction summary and push the data
+    transSummary.forEach((transaction) => {
+      if (transaction.totalAmount !== null) {
+        chartData.push([transaction.transactionType, transaction.totalAmount]);
+      }
+    });
+    
+    return chartData;
   };
   return (
     <div className="upload-page">
@@ -17,7 +70,8 @@ export default function UploadPage() {
         name="file"
         types={fileTypes}
       />
-      <p>{file ? `File name: ${file.name}` : "No Files"}</p>
+      <p>{message ? `${file.name} ` + message : null}</p>
     </div>
+    
   );
 }
